1d23a4892f18007c113aec51361661569662c666
Kazuya Iwami <kiiwami@amazon.com>
2019-12-28 18:08:55 +0900

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -5,14 +5,8 @@
 
 # The argument to this script is the image name. This will be used as the image on the local
 # machine and combined with the account and region to form the repository name for ECR.
-image=$1
+imagename=pytorch-cifar10
 set -x
-
-if [ "$image" == "" ]
-then
-    echo "Usage: $0 <image-name>"
-    exit 1
-fi
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
@@ -28,15 +22,15 @@ region=$(aws configure get region)
 region=${region:-ap-northeast-1}
 
 
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${imagename}:latest"
 
 # If the repository doesn't exist in ECR, create it.
 
-aws ecr describe-repositories --repository-names "${image}" > /dev/null 2>&1
+aws ecr describe-repositories --repository-names "${imagename}" > /dev/null 2>&1
 
 if [ $? -ne 0 ]
 then
-    aws ecr create-repository --repository-name "${image}" > /dev/null
+    aws ecr create-repository --repository-name "${imagename}" > /dev/null
 fi
 
 # Get the login command from ECR and execute it directly
@@ -48,8 +42,8 @@ $(aws ecr get-login --registry-ids 520713654638 --region ${region} --no-include-
 # Build the docker image locally with the image name and then push it to ECR
 # with the full name.
 
-docker build  -t ${image} . --build-arg REGION=${region}
-docker tag ${image} ${fullname}
+docker build  -t ${imagename} . --build-arg REGION=${region}
+docker tag ${imagename} ${fullname}
 
 docker push ${fullname}
 
